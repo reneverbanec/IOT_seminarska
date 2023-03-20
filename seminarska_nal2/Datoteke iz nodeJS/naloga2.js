95bc90f1341362c5e05a929e8f14739ad1bd7cde
reneverbanec <60441479+reneverbanec@users.noreply.github.com>
2023-03-20 13:28:47 +0100

    Add files via upload

---
+++ seminarska_nal2/Datoteke iz nodeJS/naloga2.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const WebSocket = require('ws');
+const http = require('http');
+const path = require('path')
+// Zgoraj so vse potrebne knjižnice
+
+const app = express(); // uporabimo express 
+const server = http.createServer(app); 
+const wss = new WebSocket.Server({ server });
+
+
+app.get('/', function(req, res){ // če je naslov "/" serviramo:
+    res.sendFile(path.join(__dirname+'/naloga2.html')); // serviramo spodnjo HTML stran
+} );
+app.use(express.static('public')); // serviramo statične datoteke
+
+// Ko se vzpostavi nov Websocket
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  // Ko dobimo sporočilo iz klienta
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    wss.clients.forEach((client) => { // Pošljemo sporočilo vsem klientom
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message, { binary: false}); // Binary false zato ker smo imeli težave in je vračalo sporočilo tipa Blob.
+      }
+    });
+  });
+  // Ob zaprtju povezave
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+// Zaženemo HTTP strežnik na portu 3000
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ seminarska_nal2/src/main.cpp
@@ -0,0 +1,93 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ArduinoJson.h>
+#include <WebSocketsClient.h>
+
+const char* ssid = "Mojca";
+const char* password = "realmadrid";
+
+const char* serverAddress = "192.168.1.13";
+const int serverPort = 3000;
+
+const int potPin = 34;
+
+WebSocketsClient webSocketClient;
+
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
+  switch (type) {
+    case WStype_DISCONNECTED: // Če je povezava prekine
+      Serial.println("WebSocket disconnected");
+      break;
+    case WStype_CONNECTED: // Ko se povezava vzpostavi
+      Serial.println("WebSocket connected");
+      break; 
+    case WStype_TEXT: // Prejeto sporočilo
+      Serial.printf("Received: %s\n", payload);
+      break;
+    default:
+      break;
+  }
+}
+
+
+void setup() {
+  pinMode(2, OUTPUT); // tu določimo, da je pin 2 izhod (vgrajena LED dioda)
+  pinMode(potPin, INPUT); // določimo da je PIN34 vhod (analogne vrednosti)
+
+  // Zagon komunikacije preko serijskih vrat oz. USB
+  Serial.begin(115200);
+
+  //Povezava z WIFI
+  Serial.println("Povezovanje");
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  // Izpišemo IP naslov
+  Serial.println("Modul ESP32 je povezan !");
+  Serial.print("IP naslov esp32 modula: ");
+  Serial.println(WiFi.localIP());
+
+  // vzpostavimo WebSocket povezavo
+  webSocketClient.begin(serverAddress, serverPort);
+  webSocketClient.onEvent(webSocketEvent);
+
+  delay(500);
+
+  // Indikacija delovanja modula
+  digitalWrite(2, 1); // na pin 2 pošljemo 1
+  delay(500);
+  digitalWrite(2, 0); // na pin 2 pošljemo 1
+  delay(500);
+  digitalWrite(2, 1); // na pin 2 pošljemo 1
+  delay(500);
+  digitalWrite(2, 0); // na pin 2 pošljemo 1
+  delay(500);
+
+
+
+}
+
+  void sendData(int value) {
+    StaticJsonDocument<64> jsonDoc;
+    jsonDoc["type"] = "potValue"; //Tip podatka
+    jsonDoc["value"] = value; // vrednost potenciometra
+    String jsonString;
+    serializeJson(jsonDoc, jsonString); // pretvorimo v JSON niz
+    Serial.println(jsonString); // izpišemmo niz
+    webSocketClient.sendTXT(jsonString); // Pošiljanje podatkov preko Websocket
+  }
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // tu zapišemo kodo, ki se ponavljajoče izvaja:
+  webSocketClient.loop();
+  int potValue = analogRead(potPin);
+  sendData(potValue);
+  delay(200);
+
+}
